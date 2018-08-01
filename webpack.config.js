79ea181f35ec1a0727ef36bbe1e65dfc00ff1e62
alexanderkif <alexander_kif@mail.ru>
2018-08-01 12:38:41 +0400

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+// Webpack v4
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader']
+          })
+      },
+      {
+        test: /\.pug$/,
+        loaders: [{
+          loader: 'apply-loader'
+        }, {
+          loader: 'pug-loader',
+          options: { pretty: true }
+        }]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/'
+            }
+        }]
+      }
+    ]
+  },
+  plugins: [ 
+    new ExtractTextPlugin(
+        {filename: 'style.css'}
+    ),
+    // new HtmlWebpackPlugin({
+    //   inject: false,
+    //   hash: true,
+    //   template: './src/index.html',
+    //   filename: 'index.html'
+    // })
+    new HtmlWebpackPlugin({
+      template: 'src/index.pug',
+      inject: false,
+      metadata: {
+        // available in index.pug under locals
+        // TODO: This doesn't work, and we need to figure out how to pass these via apply-loader
+        // title, server, baseUrl
+      }
+    })
+  ]
+}
