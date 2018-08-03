63e7c5ef64f4439d8f102f4ef81fa2522f08a0a5
alexanderkif <alexander_kif@mail.ru>
2018-08-03 18:35:14 +0400

    Added percentages

--- dist/main.js
+++ dist/main.js
@@ -86,6 +86,17 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./src/buttons/buttons.js":
+/*!********************************!*\
+  !*** ./src/buttons/buttons.js ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("$(\".btn\").click(function (e) {\n\n  // Remove any old one\n  $(\".ripple\").remove();\n\n  // Setup\n  var posX = $(this).offset().left,\n      posY = $(this).offset().top,\n      buttonWidth = $(this).width(),\n      buttonHeight = $(this).height();\n\n  // Add the element\n  $(this).prepend(\"<span class='ripple'></span>\");\n\n  // Make it round!\n  if (buttonWidth >= buttonHeight) {\n    buttonHeight = buttonWidth;\n  } else {\n    buttonWidth = buttonHeight;\n  }\n\n  // Get the center of the element\n  var x = e.pageX - posX - buttonWidth / 2;\n  var y = e.pageY - posY - buttonHeight / 2;\n\n  // Add the ripples CSS and start the animation\n  $(\".ripple\").css({\n    width: buttonWidth,\n    height: buttonHeight,\n    top: y + 'px',\n    left: x + 'px'\n  }).addClass(\"rippleEffect\");\n});\n\n//# sourceURL=webpack:///./src/buttons/buttons.js?");
+
+/***/ }),
+
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
@@ -94,7 +105,18 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scss/main.scss */ \"./src/scss/main.scss\");\n/* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_main_scss__WEBPACK_IMPORTED_MODULE_0__);\n\n\n$(\".btn\").click(function (e) {\n\n  // Remove any old one\n  $(\".ripple\").remove();\n\n  // Setup\n  var posX = $(this).offset().left,\n      posY = $(this).offset().top,\n      buttonWidth = $(this).width(),\n      buttonHeight = $(this).height();\n\n  // Add the element\n  $(this).prepend(\"<span class='ripple'></span>\");\n\n  // Make it round!\n  if (buttonWidth >= buttonHeight) {\n    buttonHeight = buttonWidth;\n  } else {\n    buttonWidth = buttonHeight;\n  }\n\n  // Get the center of the element\n  var x = e.pageX - posX - buttonWidth / 2;\n  var y = e.pageY - posY - buttonHeight / 2;\n\n  // Add the ripples CSS and start the animation\n  $(\".ripple\").css({\n    width: buttonWidth,\n    height: buttonHeight,\n    top: y + 'px',\n    left: x + 'px'\n  }).addClass(\"rippleEffect\");\n});\n\nconsole.log(\"Run flatUI...\");\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scss/main.scss */ \"./src/scss/main.scss\");\n/* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_main_scss__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _buttons_buttons__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./buttons/buttons */ \"./src/buttons/buttons.js\");\n/* harmony import */ var _buttons_buttons__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_buttons_buttons__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _percentages_percentages__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./percentages/percentages */ \"./src/percentages/percentages.js\");\n/* harmony import */ var _percentages_percentages__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_percentages_percentages__WEBPACK_IMPORTED_MODULE_2__);\n\n\n\n\nconsole.log(\"Run flatUI...\");\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/percentages/percentages.js":
+/*!****************************************!*\
+  !*** ./src/percentages/percentages.js ***!
+  \****************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("var canvas = document.getElementById('cnvs');\nvar ctx = canvas.getContext('2d');\nvar p1 = canvas.getAttribute(\"p1\");\nvar p2 = canvas.getAttribute(\"p2\");\nvar p3 = canvas.getAttribute(\"p3\");\nvar a1 = 4.71 + p1 * 2 * Math.PI / 100;\nvar a2 = a1 + p2 * 2 * Math.PI / 100;\nvar a3 = a2 + p3 * 2 * Math.PI / 100;\n\nctx.lineWidth = 65; // толщина линии\n\nctx.strokeStyle = \"#747474\";\nctx.beginPath();\nctx.arc(200, 200, 140, 4.71, a1, false);\nctx.stroke();\n\nctx.strokeStyle = \"#e75735\";\nctx.beginPath();\nctx.arc(200, 200, 140, a1, a2, false);\nctx.stroke();\n\nctx.strokeStyle = \"#4eb7a8\";\nctx.beginPath();\nctx.arc(200, 200, 140, a2, a3, false);\nctx.stroke();\n\nctx.strokeStyle = \"#e5e5e5\";\nctx.beginPath();\nctx.arc(200, 200, 140, a3, 4.71, false);\nctx.stroke();\n\n//# sourceURL=webpack:///./src/percentages/percentages.js?");
 
 /***/ }),
 
---
+++ src/percentages/percentages.js
@@ -0,0 +1,30 @@
+var canvas=document.getElementById('cnvs');
+var ctx = canvas.getContext('2d');
+var p1 = canvas.getAttribute("p1");
+var p2 = canvas.getAttribute("p2");
+var p3 = canvas.getAttribute("p3");
+var a1 = 4.71+p1*2*Math.PI/100;
+var a2 = a1+p2*2*Math.PI/100;
+var a3 = a2+p3*2*Math.PI/100;
+
+ctx.lineWidth = 65; // толщина линии
+
+ctx.strokeStyle="#747474";
+ctx.beginPath();
+ctx.arc(200, 200, 140, 4.71, a1, false);
+ctx.stroke();
+
+ctx.strokeStyle="#e75735";
+ctx.beginPath();
+ctx.arc(200, 200, 140, a1, a2, false);
+ctx.stroke();
+
+ctx.strokeStyle="#4eb7a8";
+ctx.beginPath();
+ctx.arc(200, 200, 140, a2, a3, false);
+ctx.stroke();
+
+ctx.strokeStyle="#e5e5e5";
+ctx.beginPath();
+ctx.arc(200, 200, 140, a3, 4.71, false);
+ctx.stroke();
