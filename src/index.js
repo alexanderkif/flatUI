4c75f3505d576b30379c7fa7690a1ab03decda66
Nikiforov <alexander_kif@mail.ru>
2018-08-31 15:58:55 +0400

    workpoint 31.08.2018

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./scss/main.scss";
 import "./buttons/buttons";
 import "./percentages/percentages";
 import "./sliders/sliders";
+import "./sliderm/sliderm";
 import "./forms/toggle";
 
 console.log("Run flatUI...");
