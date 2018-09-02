b87ee7ef78a0041f3fbf94b1f906816010aa91a5
alexanderkif <alexander_kif@mail.ru>
2018-09-02 20:53:11 +0400

    SliderM added moving, scale, hint-text

--- src/sliderm/sliderm.js
+++ src/sliderm/sliderm.js
@@ -1,11 +1,81 @@
-var slidermHintWidth = $('.sliderm-hint').width();
-var slidermHintHeight = $('.sliderm-hint').height();
-var slidermHintArrowSide = $('.sliderm-hint-arrow').height();
-var slidermHintArrowKatet = Math.sqrt(slidermHintArrowSide*slidermHintArrowSide/2);
 
-var elems = $('.sliderm-hint-arrow');
+var elems = $('.sliderm-body');
 [].forEach.call(elems, element => {
-    element.style.top = (slidermHintHeight - slidermHintArrowSide/2) + "px";
-    element.style.left = (slidermHintWidth/2 - slidermHintArrowSide/2) + "px";
-    element.parentElement.style.top = - slidermHintHeight - slidermHintArrowKatet - 5 + "px";
+    var hint = element.querySelector('.sliderm-hint');
+    var hintArrow = element.querySelector('.sliderm-hint-arrow');
+    var point = element.querySelector('.sliderm-point');
+    var lineActive = element.querySelector('.sliderm-line-active');
+    var line = element.querySelector('.sliderm-line');
+    var value = element.querySelector('.sliderm-value').textContent;
+    var scale = element.querySelector('.sliderm-scale');
+    var max = element.querySelector('.sliderm-max').textContent;
+    var intervals = element.querySelector('.sliderm-intervals').textContent;
+    var text = element.querySelector('.sliderm-hint-text');
+    
+    for(var i = 0; i <= intervals; i++) {
+        var digit = document.createElement('div');
+        digit.innerHTML = Math.round(max*i/intervals);
+        scale.appendChild(digit);
+        if (i==intervals) {
+            scale.style.width = scale.offsetWidth + digit.offsetWidth + "px";
+        };
+    };
+    
+
+    var draw = function() {
+        lineActive.style.width = element.clientWidth*value/max;
+        point.style.left = element.clientWidth*value/max - point.clientWidth/2;
+        text.innerHTML = Math.round(value);
+        
+        hintArrow.style.top = (hint.clientHeight - hintArrow.clientHeight/2) + "px";
+        hintArrow.style.left = (hint.clientWidth/2 - hintArrow.clientHeight/2) + "px";
+        hint.style.top = - hint.clientHeight - Math.sqrt(hintArrow.clientHeight*hintArrow.clientHeight/2) - 3 + "px";
+        hint.style.left = point.clientWidth/2 - hint.clientWidth/2 + "px";
+    };
+    
+    draw();
+
+    line.onmousedown = function(e) {
+        var lineCoords = getCoords(line);
+        var shiftX = e.pageX - lineCoords.left;
+        value = (shiftX)*max/element.clientWidth;
+        draw();
+
+        var pointCoords = getCoords(point);
+        shiftX = e.pageX - pointCoords.left;
+        var elementCoords = getCoords(element);
+
+        document.onmousemove = function(e) {
+            var newLeft = e.pageX - shiftX - elementCoords.left;
+
+            if (newLeft < -point.clientWidth/2) {
+            newLeft = -point.clientWidth/2;
+            }
+            var rightEdge = element.offsetWidth - point.offsetWidth + point.clientWidth/2;
+            if (newLeft > rightEdge) {
+            newLeft = rightEdge;
+            }
+
+            value = (newLeft + point.clientWidth/2)*max/element.clientWidth;
+            draw();
+        }
+
+        document.onmouseup = function() {
+            document.onmousemove = document.onmouseup = null;
+        };
+
+        return false; // disable selection start (cursor change)
+    };
+
+    line.ondragstart = function() {
+        return false;
+    };
+
+    function getCoords(elem) {
+        var box = elem.getBoundingClientRect();
+        return {
+            top: box.top + pageYOffset,
+            left: box.left + pageXOffset
+        };
+    };
 });
