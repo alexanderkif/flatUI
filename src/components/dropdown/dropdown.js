1a3b9acdeaadcd543a678f3cca1ee94cd4ec3fa4
Nikiforov <alexander_kif@mail.ru>
2018-12-06 16:53:31 +0400

    moved components in its own folder #2

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,11 +1,11 @@
 dropdownOpen = function () {
-    var dropdown = event.currentTarget.getElementsByClassName('dropdown__menu-1')[0];
+    var dropdown = event.currentTarget.getElementsByClassName('dropdown__menu')[0];
     dropdown.classList.toggle('visible');
     event.stopPropagation();
 }
 
 window.addEventListener('click', function() {
-    var dropdowns = document.getElementsByClassName("dropdown__menu-1");
+    var dropdowns = document.getElementsByClassName("dropdown__menu");
     for (var i = 0; i < dropdowns.length; i++) {
         dropdowns[i].classList.remove('visible');
     }
