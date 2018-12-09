39841ccc7e8240283224d3f53d9e078127250f2a
alexanderkif <alexander_kif@mail.ru>
2018-12-09 17:15:26 +0400

    exchange background and color #4

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -3,18 +3,16 @@ const rippleElements = document.getElementsByClassName("btn");
 
 for(let i = 0; i < rippleElements.length; i++) {
   rippleElements[i].onclick = function(e){
-    if (!$(this).hasClass('disabled')){
-      let X = e.pageX - this.offsetLeft;
-      let Y = e.pageY - this.offsetTop;
-      let rippleDiv = document.createElement("div");
-      rippleDiv.classList.add('ripple');
-      rippleDiv.setAttribute("style","top:"+Y+"px; left:"+X+"px;");
-      let customColor = this.getAttribute('ripple-color');
-      if (customColor) rippleDiv.style.background = customColor;
-      this.appendChild(rippleDiv);
-      setTimeout(function(){
-        rippleDiv.parentElement.removeChild(rippleDiv);
-      }, 900);
-    }
+    let X = e.pageX - this.offsetLeft;
+    let Y = e.pageY - this.offsetTop;
+    let rippleDiv = document.createElement("div");
+    rippleDiv.classList.add('ripple');
+    rippleDiv.setAttribute("style","top:"+Y+"px; left:"+X+"px;");
+    let customColor = this.getAttribute('ripple-color');
+    if (customColor) rippleDiv.style.background = customColor;
+    this.appendChild(rippleDiv);
+    setTimeout(function(){
+      rippleDiv.parentElement.removeChild(rippleDiv);
+    }, 900);
   }
 }
