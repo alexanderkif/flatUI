3e67900496823cdeb0fa9706666fff94b5069578
alexanderkif <alexander_kif@mail.ru>
2018-12-12 22:08:13 +0400

    dropdown modified #8

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -2,6 +2,11 @@ dropdownOpen = function () {
     var dropdown = event.currentTarget.getElementsByClassName('dropdown__menu')[0];
     dropdown.classList.toggle('visible');
     event.stopPropagation();
+    dropdown.addEventListener('click', function() {
+        if (event.target.firstChild.data) {
+            dropdown.parentElement.getElementsByClassName('dropdown__input')[0].value = event.target.firstChild.data;
+        }
+    })
 }
 
 window.addEventListener('click', function() {
