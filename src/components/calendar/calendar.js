26b38500bce224fc88fed51eeaeaef601d22a8e8
alexanderkif <alexander_kif@mail.ru>
2018-12-16 14:36:45 +0400

    calendar. changed today button behavior. added onChangeMonthYear #9

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -1,8 +1,27 @@
 
+$.datepicker._gotoToday = function(id) {
+    var target = jQuery(id);
+    var inst = this._getInst(target[0]);
+    if (this._get(inst, 'gotoCurrent') && inst.currentDay) {
+            inst.selectedDay = inst.currentDay;
+            inst.drawMonth = inst.selectedMonth = inst.currentMonth;
+            inst.drawYear = inst.selectedYear = inst.currentYear;
+    }
+    else {
+            var date = new Date();
+            inst.selectedDay = date.getDate();
+            inst.drawMonth = inst.selectedMonth = date.getMonth();
+            inst.drawYear = inst.selectedYear = date.getFullYear();
+            this._setDateDatepicker(target, date);
+            this._selectDate(id, this._getDateDatepicker(target));
+    }
+    this._notifyChange(inst);
+    this._adjustDate(target);
+};
+
 var mydatepickers = document.getElementsByClassName('mydatepicker');
 
 [].forEach.call(mydatepickers, function(mydatepicker) {
-
     $(mydatepicker).datepicker({
         inline: true,
         firstDay: 1,
@@ -13,6 +32,10 @@ var mydatepickers = document.getElementsByClassName('mydatepicker');
         onSelect: function () {
             var day = this.parentElement.getElementsByClassName('calendar__date')[0];
             day.innerHTML = parseInt($(this).val().split('/')[1]);
+        },
+        onChangeMonthYear: function(y, m, i){                                
+            var d = i.selectedDay;
+            $(this).datepicker('setDate', new Date(y, m - 1, d));
         }
-    }).datepicker("setDate", `${mydatepicker.dataset.month}/${mydatepicker.dataset.date}/${mydatepicker.dataset.year}`)
+    }).datepicker("setDate", `${mydatepicker.dataset.month}/${mydatepicker.dataset.date}/${mydatepicker.dataset.year}`);
 });
