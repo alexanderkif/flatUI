c7323bd22f83b90cf594ae736c7bba5cb1d11349
alexanderkif <alexander_kif@mail.ru>
2018-12-16 15:48:09 +0400

    created class Calendar. removed highlight from today #9

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -19,23 +19,25 @@ $.datepicker._gotoToday = function(id) {
     this._adjustDate(target);
 };
 
-var mydatepickers = document.getElementsByClassName('mydatepicker');
+class Calendar {
+    constructor(mydatepicker) {
+        $(mydatepicker).datepicker({
+            inline: true,
+            firstDay: 1,
+            showOtherMonths: true,
+            showButtonPanel: true,
+            showYear: false,
+            dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            onSelect: function () {
+                var day = this.parentElement.getElementsByClassName('calendar__date')[0];
+                day.innerHTML = parseInt($(this).val().split('/')[1]);
+            },
+            onChangeMonthYear: function(y, m, i){                                
+                var d = i.selectedDay;
+                $(this).datepicker('setDate', new Date(y, m - 1, d));
+            }
+        }).datepicker("setDate", `${mydatepicker.dataset.month}/${mydatepicker.dataset.date}/${mydatepicker.dataset.year}`);
+    }
+}
 
-[].forEach.call(mydatepickers, function(mydatepicker) {
-    $(mydatepicker).datepicker({
-        inline: true,
-        firstDay: 1,
-        showOtherMonths: true,
-        showButtonPanel: true,
-        showYear: false,
-        dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-        onSelect: function () {
-            var day = this.parentElement.getElementsByClassName('calendar__date')[0];
-            day.innerHTML = parseInt($(this).val().split('/')[1]);
-        },
-        onChangeMonthYear: function(y, m, i){                                
-            var d = i.selectedDay;
-            $(this).datepicker('setDate', new Date(y, m - 1, d));
-        }
-    }).datepicker("setDate", `${mydatepicker.dataset.month}/${mydatepicker.dataset.date}/${mydatepicker.dataset.year}`);
-});
+[].forEach.call(document.getElementsByClassName('mydatepicker'), element => new Calendar(element));
