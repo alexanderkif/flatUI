2c631a992351b2f2a914ba925a0fb43e201dfdee
Nikiforov <alexander_kif@mail.ru>
2018-12-20 11:28:14 +0400

    form-input.js refactoring #10

--- src/components/form-input/form-input.js
+++ src/components/form-input/form-input.js
@@ -1,33 +1,30 @@
 
 class FormInput {
     constructor(element) {
-        this.reg = element.dataset.reg;
-        this.success = element.dataset.success;
-        this.error = element.dataset.error;
-        this.textDiv = element.parentElement.getElementsByClassName('form-input__text')[0];
-        this.buttonDiv = element.parentElement.getElementsByClassName('form-input__button')[0];
-
-        element.addEventListener('input', function() {
-            if (this.textDiv.value.length==0) {
-                this.buttonDiv.style.display = 'none';
-                return;
-            }
-            this.buttonDiv.style.display = 'flex';
-            FormInput.check(this);
-        }.bind(this));
+        this.textDiv = $('.form-input__text', element)[0];
+        this.buttonDiv = $('.form-input__button', element)[0];
+        this.reg = this.textDiv.dataset.reg;
+        this.success = this.textDiv.dataset.success;
+        this.error = this.textDiv.dataset.error;
+        element.addEventListener('input', this.checkInput.bind(this));
     }
-
-    static check(formInput) {
-        var re = new RegExp(formInput.reg);
-        if (re.test(formInput.textDiv.value)) {
-            formInput.buttonDiv.classList.remove("form-input__button_error");
-            formInput.buttonDiv.innerHTML = formInput.success;
+    
+    checkInput() {
+        if (this.textDiv.value.length==0) {
+            this.buttonDiv.style.display = 'none';
+            return;
+        }
+        this.buttonDiv.style.display = 'flex';
+        var re = new RegExp(this.reg);
+        if (re.test(this.textDiv.value)) {
+            this.buttonDiv.classList.remove("form-input__button_error");
+            this.buttonDiv.innerHTML = this.success;
         }
         else {
-            formInput.buttonDiv.classList.add("form-input__button_error");
-            formInput.buttonDiv.innerHTML = formInput.error;
+            this.buttonDiv.classList.add("form-input__button_error");
+            this.buttonDiv.innerHTML = this.error;
         }
     }
 }
 
-$('.form-input__text').each((index,element) => new FormInput(element));
+$('.form-input').each((index,element) => new FormInput(element));
