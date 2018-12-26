52be1a5ecf69b77e950b32d89ddfd8a8a63656ff
alexanderkif <alexander_kif@mail.ru>
2018-12-26 23:48:16 +0400

    Updates

---
+++ js/dropdown.js
@@ -0,0 +1,24 @@
+
+class Dropdown {
+    constructor(element) {
+        element.addEventListener('click', function () {
+            var dropdown = event.currentTarget.getElementsByClassName('dropdown__menu')[0];
+            dropdown.classList.toggle('visible');
+            event.stopPropagation();
+            dropdown.addEventListener('click', function() {
+                if (event.target.firstChild.data) {
+                    dropdown.parentElement.getElementsByClassName('dropdown__input')[0].value = event.target.firstChild.data;
+                }
+            })
+        })
+    }
+}
+
+$('.dropdown').each((index,element) => new Dropdown(element));
+
+window.addEventListener('click', function() {
+    var dropdowns = document.getElementsByClassName("dropdown__menu");
+    for (var i = 0; i < dropdowns.length; i++) {
+        dropdowns[i].classList.remove('visible');
+    }
+})
