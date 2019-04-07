674047d51397690201166ccbcd499278af7f6b35
alexanderkif <alexander_kif@mail.ru>
2019-04-07 19:02:49 +0400

    install jquery and icons #19

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 
+import 'jquery-ui-dist/jquery-ui';
+import 'lc-switch/lc_switch.min';
+import 'lc-switch/lc_switch.css';
+import 'modifiable-slider/dist/main';
+import 'modifiable-slider/dist/sliderm3.css';
 import './scss/main.scss';
 import './img/arrow.png';
 import './img/city.png';
@@ -6,14 +11,10 @@ import './img/john.png';
 import './img/sarah.png';
 import './img/velo.png';
 import './img/favicon.ico';
-import 'lc-switch/lc_switch.min';
-import 'lc-switch/lc_switch.css';
 import './components/button/button';
 import './components/form-input/form-input';
 import './components/dropdown/dropdown';
 import './components/search/search';
 import './components/calendar/calendar';
 import './components/sectors/sectors';
-import 'modifiable-slider/dist/main';
-import 'modifiable-slider/dist/sliderm3.css';
 import './components/form-toggle/form-toggle';
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
-// Webpack v4
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: { main: './src/index.js' },
@@ -24,13 +25,9 @@ module.exports = {
                     {
                         loader: MiniCssExtractPlugin.loader,
                         options: {
-                            // you can specify a publicPath here
-                            // by default it use publicPath in webpackOptions.output
-                            // publicPath: '../'
                         },
                     },
                     'css-loader',
-                    // 'postcss-loader',
                     'sass-loader',
                 ],
             },
@@ -54,7 +51,6 @@ module.exports = {
             },
             {
                 test: /\.(jpe?g|png|gif|svg)$/i,
-                // loader: "file-loader?name=/img/[name].[ext]"
                 exclude: /fonts/,
                 use: [{
                     loader: 'file-loader',
@@ -78,15 +74,11 @@ module.exports = {
     },
     plugins: [
         new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
             filename: '[name].css',
-            // chunkFilename: "[id].css"
         }),
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: 'src/index.pug',
-            // inject: false,
             metadata: {
             },
         }),
@@ -109,6 +101,10 @@ module.exports = {
         new HtmlWebpackPlugin({
             filename: '5.html',
             template: 'src/html/5.pug',
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
         }),
     ],
     resolve: {
