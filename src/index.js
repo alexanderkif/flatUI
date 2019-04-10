1421bd033929a1c7b6605b647c3f135184f18892
Nikiforov <alexander_kif@mail.ru>
2019-04-10 14:19:36 +0400

    autoimport modified #18

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 
-import './scss/constants';
-
-require.context('./', true, /\.scss$/);
-
 function importAll(r) {
     r.keys().forEach(r);
 }
-
+importAll(require.context('./vendors/', true, /\.js$/));
+require.context('./', true, /\.scss$/);
 importAll(require.context('./components/', true, /\.js$/));
+require.context('./img/', true, /\.(png|ico)$/);
---
+++ src/vendors/vendors.js
@@ -0,0 +1,6 @@
+
+import 'jquery-ui-dist/jquery-ui';
+import 'lc-switch/lc_switch.min';
+import 'lc-switch/lc_switch.css';
+import 'modifiable-slider/dist/main';
+import 'modifiable-slider/dist/sliderm3.css';
